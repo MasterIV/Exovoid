872f9e94272e113d9e199b9542338d7d4eeb003d
Marcel Zons <marcel.zons@googlemail.com>
2024-09-27 17:13:15 +0200

    Updated Edge calculation

--- src/logic/calculateEdge.ts
+++ src/logic/calculateEdge.ts
@@ -2,5 +2,6 @@ import CharacterType from "../types/character";
 
 export default function calculateEdge(stats: CharacterType) {
     const COO = stats.attributes.COO || 0;
-    return  Math.ceil(4 + COO / 2);
+	const EDU = stats.attributes.EDU || 0;
+    return  Math.ceil(3 + COO / 4 + EDU / 2);
 }
