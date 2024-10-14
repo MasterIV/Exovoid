62d0c375c0f0ca4025c90cc15cf2942cb6f52a71
Tobias Rojahn <tobias@rojahn.org>
2024-10-14 19:54:36 +0200

    Change website title and disable character fallback

--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,6 @@ import {io} from "socket.io-client";
 import {ServerSocket} from "./types/server";
 import AccountType from "./types/account";
 import CharacterType from "./types/character";
-import defaults from "./data/character.json";
 
 const socket: ServerSocket = io();
 
@@ -16,8 +15,6 @@ socket.onAny(console.log);
 export const onCharacterChange = (setCharacter: (data: CharacterType) => void) => {
     socket.removeAllListeners("character");
     socket.on("character", setCharacter);
-    // Load some dummy data if we don't have a backend running
-    socket.on("connect_error", () => setCharacter(defaults));
 }
 
 export const onAccountChange = (setAccount: (data: AccountType) => void) => {
